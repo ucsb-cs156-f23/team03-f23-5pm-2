ae6e820e21e33660a2fc49f47f0e9e24c6e1ba44
ChinDandekar <102712851+ChinDandekar@users.noreply.github.com>
2023-11-02 17:03:16 -0700

    Added ucsbDiningCommonsMenuItemsFixtures.js

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,38 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem:
+    [
+      {
+            "id": 1,
+            "diningCommonCode": "De La Guerra",
+            "name": "Pasta Primavera",
+            "station": "To Order"      
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "id": 2,
+            "diningCommonCode": "De La Carillo",
+            "name": "Pasta Primadora",
+            "station": "To mongolia"       
+        },
+
+        {
+            "id": 3,
+            "diningCommonCode": "De La Portola",
+            "name": "Pasta Pesto",
+            "station": "To east side"       
+        },
+
+        {
+            "id": 3,
+            "diningCommonCode": "De La",
+            "name": "Pasta",
+            "station": "To"       
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
