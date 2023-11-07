f0eb9a6d295bf385db082b0c45808e3c8244e75f
Hancheng Li <hanchengli@ucsb.edu>
2023-11-07 13:37:11 -0800

    created menuitemreview fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,39 @@
+const ucsbDatesFixtures = {
+    oneReview: {
+        "id": 1,
+        "itemId": 10,
+        "reviewerEmail": "hanchengli@ucsb.edu",
+        "stars": 5,
+        "dateReviewed": "2022-01-02T12:00:00",
+        "comments": "best of the best"
+    },
+    threeReviews: [
+        {
+            "id": 1,
+            "itemId": 10,
+            "reviewerEmail": "hanchengli@ucsb.edu",
+            "stars": 5,
+            "dateReviewed": "2022-01-02T12:00:00",
+            "comments": "best of the best"
+        },
+        {
+            "id": 2,
+            "itemId": 15,
+            "reviewerEmail": "cgaucho@ucsb.edu",
+            "stars": 3,
+            "dateReviewed": "2022-01-15T12:00:00",
+            "comments": "mediocre"
+        },
+        {
+            "id": 3,
+            "itemId": 26,
+            "reviewerEmail": "random@ucsb.edu",
+            "stars": 1,
+            "dateReviewed": "2022-02-15T12:00:00",
+            "comments": "taste like shait"
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
