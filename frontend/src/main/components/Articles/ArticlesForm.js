7da0c3d989f5816eead0721fcb2ca4eb3aa69440
Amgad Hawari <amgadhawari@169-231-137-85.wireless.ucsb.edu>
2023-11-08 16:41:45 -0800

    Corrected Article Form

--- frontend/src/main/components/Articles/ArticlesForm.js
+++ frontend/src/main/components/Articles/ArticlesForm.js
@@ -20,8 +20,9 @@ function ArticlesForm({ initialContents, submitAction, buttonLabel = "Create" })
     // Note that even this complex regex may still need some tweaks
 
     // Stryker disable next-line Regex
-    
-    
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    // Stryker disable next-line all
 
     return (
 
@@ -29,14 +30,13 @@ function ArticlesForm({ initialContents, submitAction, buttonLabel = "Create" })
 
 
             <Row>
-                /</Row>
 
                 {initialContents && (
                     <Col>
                         <Form.Group className="mb-3" >
                             <Form.Label htmlFor="id">Id</Form.Label>
                             <Form.Control
-                                data-testid= {ArticlesForm + "id"}
+                                data-testid="ArticlesForm-id"
                                 id="id"
                                 type="text"
                                 {...register("id")}
@@ -50,66 +50,108 @@ function ArticlesForm({ initialContents, submitAction, buttonLabel = "Create" })
                 <Col>
                     <Form.Group className="mb-3" >
                         <Form.Label htmlFor="title">Title</Form.Label>
-                        <Form
-                            data-testid= {ArticlesForm + "-title"} 
-                            id= "title"
+                        <Form.Control
+                            data-testid="ArticlesForm-title"
+                            id="title"
                             type="text"
                             isInvalid={Boolean(errors.title)}
-                            {...register("title", { required: "Title is required.", maxlength: {value:20, message: "Max lenght of title is 20 characters" })}
+                            {...register("title", {
+                                required: "Title is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.title?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="url">URL</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-url"
+                            id="url"
+                            type="text"
+                            isInvalid={Boolean(errors.title)}
+                            {...register("url", {
+                                required: "URL is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.url?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group> 
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-explanation"
+                            id="explanation"
+                            type="text"
+                            isInvalid={Boolean(errors.title)}
+                            {...register("explanation", {
+                                required: "Explanation is required."
+                            })}
                         />
                         <Form.Control.Feedback type="invalid">
                             {errors.explanation?.message}
-                </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="email">Email</Form.Label>
-                <Form.Control
-                    data-testid={ArticleForm + "-email"}
-                    id="email"
-                    type="text"
-                    isInvalid={Boolean(errors.email)}
-                    {...register("email", {
-                        required: "Email is required."
-                    })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.email.message}
-                </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3" >
-                <Form.Label htmlFor="dateAdded">Date Added</Form.Label>
-                <Form.Control
-                    data-testid={ArticleForm + "-dateAdded"}
-                    id="dateAdded"
-                    type="datetime-local"
-                    isInvalid={Boolean(errors.dateAdded)}
-                    {...register("dateAdded", {
-                        required: "Date Added is required."
-                    })}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.dateAdded.message}
-                </Form.Control.Feedback>
-            </Form.Group>
-
-
-
-            <Button
-                type="submit"
-                data-testid={ArticleForm + "-submit"}
-            >
-                {buttonLabel}
-            </Button>
-            <Button
-                variant="Secondary"
-                onClick={() => navigate(-1)}
-                data-testid={ArticleForm + "-cancel"}
-            >
-                Cancel
-            </Button>
-          </Col>
+                        </Form.Control.Feedback>
+                    </Form.Group> 
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-email"
+                            id="email"
+                            type="text"
+                            isInvalid={Boolean(errors.title)}
+                            {...register("email", {
+                                required: "Email is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.email?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group> 
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateAdded">Date (iso format)</Form.Label>
+                        <Form.Control
+                            data-testid="ArticlesForm-dateAdded"
+                            id="dateAdded"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.localDateTime)}
+                            {...register("dateAdded", { required: true, pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.dateAdded && 'DateAdded is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button
+                        type="submit"
+                        data-testid="ArticlesForm-submit"
+                    >
+                        {buttonLabel}
+                    </Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="ArticlesForm-cancel"
+                    >
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
         </Form>
 
     )
