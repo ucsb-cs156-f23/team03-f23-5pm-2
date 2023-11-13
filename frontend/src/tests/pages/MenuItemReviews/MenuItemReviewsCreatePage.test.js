19805d2b367013c5d61600665178fed3c2872583
Hancheng Li <hanchengli@ucsb.edu>
2023-11-13 00:58:34 -0800

    Create Page and tests modified correctly for MenuItemReview

--- frontend/src/tests/pages/MenuItemReviews/MenuItemReviewsCreatePage.test.js
+++ frontend/src/tests/pages/MenuItemReviews/MenuItemReviewsCreatePage.test.js
@@ -30,38 +30,6 @@ jest.mock('react-router-dom', () => {
 
 describe("MenuItemReviewsCreatePage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
-
-    const setupUserOnly = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
-
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
-
-        setupUserOnly();
-       
-        // act
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <MenuItemReviewsCreatePage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
-    });
-
-});
-
-describe("MenuItemReviewsCreatePage tests", () => {
-
     const axiosMock =new AxiosMockAdapter(axios);
 
     beforeEach(() => {
